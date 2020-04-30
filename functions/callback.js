e7bce0dd881710a05eeb4a5e4b1939991dc10a9f
Ethan DeGuire <30885052+ethandeguire@users.noreply.github.com>
2020-04-30 09:21:24 -0400

    Update callback.js

--- functions/callback.js
+++ functions/callback.js
@@ -24,7 +24,7 @@ exports.handler = function (event, context, callback) {
 
     console.log(authOptions)
 
-    request.post(authOptions, function (error, response, body) {
+    return request.post(authOptions, function (error, response, body) {
         if (!error && response.statusCode === 200) {
             console.log(error, response, body)
 
